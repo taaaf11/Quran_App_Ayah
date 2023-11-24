bee21821861ca821d55016c3c45cfb43758e116a
taaaf11 <taafuuu@gmail.com>
2023-11-24 21:51:42 +0500

    Application UI ready

--- main.py
+++ main.py
@@ -4,11 +4,39 @@ import flet as ft
 
 def main(page):
     page.title = 'Quran Ayah by Ayah'
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.fonts = {}
     
     quran_text = ft.Text('a', size=10)
+    surah_aya_field = ft.TextField(hint_text='Surah:Ayah', label='Index')
     
+    def get_text(e):
+        index = surah_aya_field.value
+        data = surah_aya[index]
+        # get arabic text
+        text = data[0]
+        # get font name
+        font = data[1]
+        # set to field
+        quran_text.font_family = f"Fonts\\{font}.ttf"
+        quran_text.value = text
+        page.update()
+        
     
-    pass
+    submit_button = ft.IconButton(ft.icons.CHECK, on_click=get_text)
+    
+    page.add(
+        ft.Column([
+            ft.Row([
+                surah_aya_field,
+                submit_button
+            ],
+            alignment=ft.MainAxisAlignment.CENTER),
+            
+            quran_text
+        ],
+        horizontal_alignment=ft.CrossAxisAlignment.CENTER)
+    )
 
 
 if __name__ == '__main__':
