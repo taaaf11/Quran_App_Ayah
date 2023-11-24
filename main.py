8bbe58e24b617f2d8bec66e47bdaa63ce09f1247
taaaf11 <taafuuu@gmail.com>
2023-11-24 21:04:03 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from aya_dict import surah_aya
+import flet as ft
+
+
+def main(page):
+    page.title = 'Quran Ayah by Ayah'
+    
+    quran_text = ft.Text('a', size=10)
+    
+    
+    pass
+
+
+if __name__ == '__main__':
+    ft.app(target=main)
